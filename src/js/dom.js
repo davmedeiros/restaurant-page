d6859d295a0c134bad5f07e25cc382c093bd1b3d
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-02 00:20:23 +0100

    Merge functions that use the same creation params

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,58 +1,29 @@
 import assetHeroImage from '../images/oc-gonzalez-PDgZtSrgM6M-unsplash.jpg';
 
-function createMenu(...items) {
+function createElement(type, text, id, ...htmlClasses) {
+  const element = document.createElement(type);
+  element.textContent = text;
+
+  htmlClasses.forEach((htmlClass) => {
+    element.classList.add(htmlClass);
+  });
+
+  if (id) element.id = id;
+
+  return element;
+}
+
+function createMenu(htmlClass, ...items) {
   const menu = document.createElement('menu');
 
   items.forEach((item) => {
     const li = document.createElement('li');
-    li.classList.add('tab');
+    li.classList.add(htmlClass);
     li.textContent = item;
     menu.appendChild(li);
   });
 
   return menu;
-}
-
-function createNav() {
-  const nav = document.createElement('nav');
-  return nav;
-}
-
-function createHeading(level, text) {
-  const heading = document.createElement(`h${level}`);
-  heading.textContent = text;
-  return heading;
-}
-
-function createHeader() {
-  const header = document.createElement('header');
-  return header;
-}
-
-function assembleHeader() {
-  const header = createHeader();
-  const h1 = createHeading(1, 'Grumpy Cook');
-  header.appendChild(h1);
-  const nav = createNav();
-  header.appendChild(nav);
-  const menu = createMenu('Home', 'Menu', 'Contact');
-  nav.appendChild(menu);
-  return header;
-}
-
-function createDiv(id) {
-  const div = document.createElement('div');
-
-  if (id) div.id = id;
-
-  return div;
-}
-
-function createFigcaption(text) {
-  const figcaption = document.createElement('figcaption');
-  // TODO: Create logic for processing hyperlinks
-  figcaption.textContent = text;
-  return figcaption;
 }
 
 function createImage(id, src, alt) {
@@ -65,25 +36,15 @@ function createImage(id, src, alt) {
   return image;
 }
 
-function createFigure(id) {
-  const figure = document.createElement('figure');
-
-  if (id) figure.id = id;
-
-  return figure;
-}
-
-function createSection(id) {
-  const section = document.createElement('section');
-
-  if (id) section.id = id;
-
-  return section;
-}
-
-function createMain() {
-  const main = document.createElement('main');
-  return main;
+function assembleHeader() {
+  const header = createElement('header');
+  const h1 = createElement(1, 'Grumpy Cook');
+  header.appendChild(h1);
+  const nav = createNav();
+  header.appendChild(nav);
+  const menu = createMenu('Home', 'Menu', 'Contact');
+  nav.appendChild(menu);
+  return header;
 }
 
 function assembleMain() {
