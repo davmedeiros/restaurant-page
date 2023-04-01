e5e8917c9b4b2f6592ce2c4757604d55eb2fabef
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-01 13:13:59 +0100

    Remove config test code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import 'normalize.css';
 import './style.css';
-
-const element = document.createElement('h1');
-element.textContent = 'Hello, World!';
-element.classList.add('hello');
-document.body.appendChild(element);
