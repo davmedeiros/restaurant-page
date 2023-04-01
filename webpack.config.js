889c0818364bf8b4525129992dfa2d910607f9be
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-01 13:41:07 +0100

    Add HTML Webpack Plugin and html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
@@ -10,6 +16,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
