f3212b23db8ef5a9546d1af2bdfcb9099edace3f
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-01 12:39:54 +0100

    Bundle css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+import './style.css';
+
+const element = document.createElement('h1');
+element.textContent = 'Hello, World!';
+element.classList.add('hello');
+document.body.appendChild(element);
