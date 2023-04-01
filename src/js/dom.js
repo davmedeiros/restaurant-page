6b6b5e75df4d47eca68cc237ac6d6348bb46893b
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-01 21:37:55 +0100

    Create createMenu function

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,5 +1,23 @@
 import assetHeroImage from '../images/oc-gonzalez-PDgZtSrgM6M-unsplash.jpg';
 
-export default function printMe() {
-  console.log('dom controller!');
+function createMenu(parent = document.body, ...items) {
+  const menu = document.createElement('menu');
+
+  items.forEach((item) => {
+    const li = document.createElement('li');
+    li.classList.add('tab');
+    li.textContent = item;
+    menu.appendChild(li);
+  });
+
+  parent.appendChild(menu);
+}
+
+function createNav(parent = document.body) {
+  const nav = document.createElement('nav');
+  parent.appendChild(nav);
+}
+
+function createHeader() {
+  const header = document.createElement('header');
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,2 @@
 import 'normalize.css';
 import '../css/style.css';
-import printMe from './dom';
-
-console.log(printMe());
