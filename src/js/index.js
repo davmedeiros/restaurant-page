73ce093615c2132435f02c255078ed82d0cc300d
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-01 19:29:26 +0100

    Style hero section

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,6 @@
 import 'normalize.css';
 import '../css/style.css';
+import HERO_IMAGE from '../images/oc-gonzalez-PDgZtSrgM6M-unsplash.jpg';
+
+const image = document.querySelector('#hero-image');
+image.src = HERO_IMAGE;
