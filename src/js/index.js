feeb6940131f951067034f03f299f69096e814d0
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-02 17:15:32 +0100

    Revert "Show Home on load"
    This reverts commit 0ce0ec2a4cc8b553d75ce0813257fa1dd54490d9.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 import 'normalize.css';
 import '../css/style.css';
 import assembleHeader from './header';
-import assembleHome from './home';
 
 function loadPage() {
   const container = document.querySelector('#content');
   container.appendChild(assembleHeader());
-  container.appendChild(assembleHome());
 }
 
 loadPage();
