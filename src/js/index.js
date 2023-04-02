0ce0ec2a4cc8b553d75ce0813257fa1dd54490d9
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-02 17:11:36 +0100

    Show Home on load

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import 'normalize.css';
 import '../css/style.css';
 import assembleHeader from './header';
+import assembleHome from './home';
 
 function loadPage() {
   const container = document.querySelector('#content');
   container.appendChild(assembleHeader());
+  container.appendChild(assembleHome());
 }
 
 loadPage();
