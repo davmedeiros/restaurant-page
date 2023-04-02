2ff73a4a219c0701165b197d7c72d66b9877d04e
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-04-02 16:45:45 +0100

    Add data to contacts and menu

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,8 +2,10 @@ import createElement from './element';
 
 function assembleContact() {
   const main = createElement('main');
-  const h2 = createElement('h2', 'Chef Gervasio Grumpy - 555-4321');
+  const h2 = createElement('h2', 'Chef Gervasio Grumpy');
   main.appendChild(h2);
+  const p = createElement('p', '555-4321');
+  main.appendChild(p);
   return main;
 }
 
--- src/js/menu.js
+++ src/js/menu.js
@@ -2,8 +2,15 @@ import createElement from './element';
 
 function assembleMenu() {
   const main = createElement('main');
-  const h2 = createElement('h2', 'Grumpy Shrimp');
+  const h2 = createElement('h2', 'Dishes');
   main.appendChild(h2);
+  const dishes = ['Grumpy Shrimp', 'Grumpy Fish'];
+
+  dishes.forEach((dish) => {
+    const p = createElement('p', dish);
+    main.appendChild(p);
+  });
+
   return main;
 }
 
