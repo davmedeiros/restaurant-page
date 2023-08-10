03f76f48df6733c0db69a6ee3600cd3dcba458f9
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-10 12:50:10 +0100

    Create buildHeader, clearContainer and loadPage

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 import '../css/style.css';
 import { loadPage } from './page-load';
 
-console.log(loadPage());
+loadPage();
--- src/js/page-load.js
+++ src/js/page-load.js
@@ -1,5 +1,32 @@
-const loadPage = (page = '') => {
-    return 'Connected';
+const clearContainer = (container) => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+const buildHeader = (container) => {
+    const header = document.createElement('header');
+    const nav = document.createElement('nav');
+    const home = document.createElement('button');
+    home.id = 'home';
+    home.textContent = home.id.toUpperCase();
+    const menu = document.createElement('button');
+    menu.id = 'menu';
+    menu.textContent = menu.id.toUpperCase();
+    const contact = document.createElement('button');
+    contact.id = 'contact';
+    contact.textContent = contact.id.toUpperCase();
+    nav.appendChild(home);
+    nav.appendChild(menu);
+    nav.appendChild(contact);
+    header.appendChild(nav);
+    container.appendChild(header);
+}
+
+const loadPage = (page) => {
+    const content = document.querySelector('#content');
+    clearContainer(content);
+    buildHeader(content);
 }
 
 export { loadPage };
